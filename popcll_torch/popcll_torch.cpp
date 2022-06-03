745cf2894827784f5c63b83d7f95440e2fb1465c
Anshuman Suri <as9rw@virginia.edu>
2022-06-03 19:53:16 -0400

    Fix multi-GPU error

--- popcll_torch/popcll_torch.cpp
+++ popcll_torch/popcll_torch.cpp
@@ -1,10 +1,11 @@
 #include <torch/extension.h>
+#include <c10/cuda/CUDAGuard.h>
 
 #include <vector>
 
 // CUDA forward declarations
 
-torch::Tensor popcll_torch_cuda_forward(
+torch::Tensor popcll_torch_cuda(
     torch::Tensor input);
 
 // C++ interface
@@ -12,21 +13,22 @@ torch::Tensor popcll_torch_cuda_forward(
 // NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
 #define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_LONG(x) AT_ASSERTM(x.type().scalarType() == at::ScalarType::Long, #x " must be a long tensor")
+#define CHECK_LONG(x) AT_ASSERTM(x.scalar_type() == at::ScalarType::Long, #x " must be a long tensor")
 #define CHECK_INPUT(x)   \
     CHECK_CUDA(x);       \
     CHECK_CONTIGUOUS(x); \
     CHECK_LONG(x)
 
-torch::Tensor popcll_torch_forward(
+torch::Tensor popcll_torch(
     torch::Tensor input)
 {
     CHECK_INPUT(input);
-
-    return popcll_torch_cuda_forward(input);
+    // Guard (useful for multi-GPU machines)
+    const at::cuda::OptionalCUDAGuard device_guard(device_of(input));
+    return popcll_torch_cuda(input);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    m.def("popcll", &popcll_torch_forward, "popcll_torch (CUDA)");
+    m.def("popcll", &popcll_torch, "popcll_torch (CUDA)");
 }
--- popcll_torch/popcll_torch_kernel.cu
+++ popcll_torch/popcll_torch_kernel.cu
@@ -8,13 +8,15 @@
 namespace
 {
     template <typename scalar_t>
-    __global__ void popcll_torch_cuda_forward_kernel(
+    __global__ void popcll_torch_cuda_kernel(
         const torch::PackedTensorAccessor<scalar_t, 1, torch::RestrictPtrTraits, size_t> input,
         torch::PackedTensorAccessor<scalar_t, 1, torch::RestrictPtrTraits, size_t> counts)
     {
-        // index
-        const int i = blockIdx.x * blockDim.x + threadIdx.x;
-        if (i < input.size(0))
+        // Opt for a grid-strided loop
+        const int index = blockIdx.x * blockDim.x + threadIdx.x;
+        const int stride = blockDim.x * gridDim.x;
+        const int n = input.size(0);
+        for (int i = index; i < n; i += stride)
         {
             // Count operation
             counts[i] = __popcll(input[i]);
@@ -22,17 +24,17 @@ namespace
     }
 } // namespace
 
-torch::Tensor popcll_torch_cuda_forward(torch::Tensor input)
+torch::Tensor popcll_torch_cuda(torch::Tensor input)
 {
     const auto batch_size = input.size(0);
 
     auto counts = torch::zeros_like(input);
 
-    const int threads = 1024;
-    const int blocks = (batch_size + threads - 1) / threads;
+    const int blockSize = 1024;
+    const int numBlocks = (batch_size + blockSize - 1) / blockSize;
 
     AT_DISPATCH_INTEGRAL_TYPES(input.type(), "popcll_torch_forward_cuda", ([&] {
-        popcll_torch_cuda_forward_kernel<scalar_t><<<blocks, threads>>>(
+        popcll_torch_cuda_kernel<scalar_t><<<numBlocks, blockSize>>>(
             input.packed_accessor<scalar_t, 1, torch::RestrictPtrTraits, size_t>(),
             counts.packed_accessor<scalar_t, 1, torch::RestrictPtrTraits, size_t>());
     }));
